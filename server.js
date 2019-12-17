a35d45c6a0049366a14eb7e29e789a1afb0b32e6
twizou <57894503+twizou@users.noreply.github.com>
2019-12-17 15:59:59 +0100

    dockerfile, docker-compose, server.js, gitignore, package*.json

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express();
+const { Client } = require('pg')
+const redis = require('redis')
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
+});
+
+client.connect().catch(e=>console.log(e));
+const redisClient = redis.createClient({ host: 'devops_redis' });
+
+app.use(express.json());
+
+
+app.get('/', function(req, res) {
+    res.json({ message: 'Hello World'});
+});
+
+app.get('/status', async function(req, res) {
+    const uptimePG = await client.query("SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime;");
+    
+    const uptime = uptimePG.rows[0].uptime
+
+    const uptimeString = () => {
+        let time = ""
+    
+        time += uptime.hours ? `${uptime.hours}h ` : ""
+        time += uptime.minutes ? `${uptime.minutes}m ` : ""
+        time += uptime.seconds ? `${uptime.seconds}s` : ""
+    
+        return time
+      }
+
+
+    res.json({ 
+        status: 'OK',
+        postgresUptime: uptimeString(),
+        redisConnectedClients:Number(redisClient.server_info.connected_clients)
+    });
+});
+
+// start the app
+app.listen(3000, function() {
+    console.log("Express is running on port 3000");
+});
